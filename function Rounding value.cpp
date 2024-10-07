209878aad1450dcafc299fe1cb12edc1057fc40c
ManasviTripathi77 <manasvi.tripathi24@sanjivani.edu.in>
2024-10-07 13:02:48 +0530

    Add files via upload

---
+++ function Rounding value.cpp
@@ -0,0 +1,9 @@
+#include<cmath>
+#include<iostream>
+using namespace std;
+int main(){
+  double num=49.0;
+  cout<<"Rounding 5.6 gives is:"<<round(5.6)<<endl;
+  cout<<"Rounding 5.4 gives is:"<<round(5.4)<<endl;
+  return 0;
+  }
